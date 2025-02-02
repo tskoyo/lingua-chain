3955d7c7112de2dc2e71cafbc407bb24ffc3e45e
tskoyo <jovansr@protonmail.com>
2025-02-02 21:11:39 +0100

    update script msg output

--- app/script/getProposal.js
+++ app/script/getProposal.js
@@ -5,7 +5,8 @@ import fs from 'fs';
 dotenv.config();
 
 const args = process.argv.slice(2);
-const proposalIndex = args[0];
+const command = args[0];
+const proposalIndex = args[1];
 
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
@@ -19,10 +20,35 @@ const governanceContract = new ethers.Contract(contractAddress, contractABI, wal
 async function getProposal(idx) {
     try {
         const proposal = await governanceContract.proposals(idx)
-        console.log(proposal);
+        printProposal(proposal);
     } catch (error) {
         console.error("Error fetching proposal:", error);
     }
 }
 
-getProposal(proposalIndex);
+async function getAllProposals() {
+    const proposalCount = await governanceContract.getProposalCount();
+
+    for (let index = 0; index < proposalCount; index++) {
+        await getProposal(index);
+    }
+}
+
+function printProposal(proposal) {
+    console.log(`Proposal ID: ${proposal.id}`);
+    console.log(`Description: ${proposal.description}`);
+    console.log(`For votes: ${proposal.forVotes}`);
+    console.log(`Against Votes: ${proposal.againstVotes}`);
+    console.log(`Proposer: ${proposal.proposer}`);
+    console.log('----------------------');
+}
+
+if (command == "getProposal" && proposalIndex != undefined) {
+    getProposal(proposalIndex);
+} else if (command == "getAllProposals") {
+    getAllProposals();
+} else {
+    console.log("Usage:");
+    console.log("  node getProposal.js getProposal <proposalIndex>");
+    console.log("  node getProposal.js getAllProposals");
+}
