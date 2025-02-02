5aca034c82237482131e04ba9ec376c9ca556168
tskoyo <jovansr@protonmail.com>
2025-02-02 17:10:42 +0100

    update get proposal script

--- app/script/getProposal.js
+++ app/script/getProposal.js
@@ -4,6 +4,9 @@ import fs from 'fs';
 
 dotenv.config();
 
+const args = process.argv.slice(2);
+const proposalIndex = args[0];
+
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
@@ -13,13 +16,13 @@ const contractABI = contractJson.abi
 
 const governanceContract = new ethers.Contract(contractAddress, contractABI, wallet);
 
-async function getProposal() {
+async function getProposal(idx) {
     try {
-        const proposal = await governanceContract.getProposalCount();
+        const proposal = await governanceContract.proposals(idx)
         console.log(proposal);
     } catch (error) {
         console.error("Error fetching proposal:", error);
     }
 }
 
-getProposal();
+getProposal(proposalIndex);
